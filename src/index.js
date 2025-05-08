82405826692df5f1982975ca7bc0dba642a8375e
Maria Escalante <maria_feminina06@yahoo.com>
2025-05-08 15:36:50 +0200

    added timezone for indicated city select options

--- src/index.js
+++ src/index.js
@@ -23,44 +23,72 @@ function updateTime() {
 
   //tokyo
   let tokyoElement = document.querySelector("#tokyo");
-  let tokyoDateElement = tokyoElement.querySelector(".date");
-  let tokyoTimeElement = tokyoElement.querySelector(".time");
-  let tokyoTime = moment.tz("Asia/Tokyo").format("HH:mm:ss");
-  tokyoDateElement.innerHTML = moment
-    .tz("Asia/Tokyo")
-    .format("dddd, DD MMM YYYY");
-  tokyoTimeElement.innerHTML = `${tokyoTime}`;
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment.tz("Asia/Tokyo").format("HH:mm:ss");
+    tokyoDateElement.innerHTML = moment
+      .tz("Asia/Tokyo")
+      .format("dddd, DD MMM YYYY");
+    tokyoTimeElement.innerHTML = `${tokyoTime}`;
+  }
 
   //hcm
   let hcmElement = document.querySelector("#hcm");
-  let hcmDateElement = hcmElement.querySelector(".date");
-  let hcmTimeElement = hcmElement.querySelector(".time");
-  let hcmTime = moment.tz("Asia/Saigon").format("HH:mm:ss");
-  hcmDateElement.innerHTML = moment
-    .tz("Asia/Saigon")
-    .format("dddd, DD MMM YYYY");
-  hcmTimeElement.innerHTML = `${hcmTime}`;
+  if (hcmElement) {
+    let hcmDateElement = hcmElement.querySelector(".date");
+    let hcmTimeElement = hcmElement.querySelector(".time");
+    let hcmTime = moment.tz("Asia/Ho_Chi_Minh").format("HH:mm:ss");
+    hcmDateElement.innerHTML = moment
+      .tz("Asia/Ho_Chi_Minh")
+      .format("dddd, DD MMM YYYY");
+    hcmTimeElement.innerHTML = `${hcmTime}`;
+  }
 
   //mnl
   let mnlElement = document.querySelector("#mnl");
-  let mnlDateElement = mnlElement.querySelector(".date");
-  let mnlTimeElement = mnlElement.querySelector(".time");
-  let mnlTime = moment.tz("Asia/Manila").format("HH:mm:ss");
-  mnlDateElement.innerHTML = moment
-    .tz("Asia/Manila")
-    .format("dddd, DD MMM YYYY");
-  mnlTimeElement.innerHTML = `${mnlTime}`;
+  if (mnlElement) {
+    let mnlDateElement = mnlElement.querySelector(".date");
+    let mnlTimeElement = mnlElement.querySelector(".time");
+    let mnlTime = moment.tz("Asia/Manila").format("HH:mm:ss");
+    mnlDateElement.innerHTML = moment
+      .tz("Asia/Manila")
+      .format("dddd, DD MMM YYYY");
+    mnlTimeElement.innerHTML = `${mnlTime}`;
+  }
 
   //ams
   let amsElement = document.querySelector("#ams");
-  let amsDateElement = amsElement.querySelector(".date");
-  let amsTimeElement = amsElement.querySelector(".time");
-  let amsTime = moment.tz("Europe/Amsterdam").format("HH:mm:ss");
-  amsDateElement.innerHTML = moment
-    .tz("Europe/Amsterdam")
-    .format("dddd, DD MMM YYYY");
-  amsTimeElement.innerHTML = `${amsTime}`;
+  if (amsElement) {
+    let amsDateElement = amsElement.querySelector(".date");
+    let amsTimeElement = amsElement.querySelector(".time");
+    let amsTime = moment.tz("Europe/Amsterdam").format("HH:mm:ss");
+    amsDateElement.innerHTML = moment
+      .tz("Europe/Amsterdam")
+      .format("dddd, DD MMM YYYY");
+    amsTimeElement.innerHTML = `${amsTime}`;
+  }
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+//dropdown
+
+function displayNewCity(event) {
+  if (event.target.value.length > 0) {
+    let timezone = event.target.value;
+    let cityName = timezone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(timezone);
+    let cityElement = document.querySelector(".cities");
+    cityElement.innerHTML = ` <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("dddd DD MMM YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("HH:mm:ss")}</div>
+        </div>`;
+  }
+}
+let citySelect = document.querySelector("#city-options");
+citySelect.addEventListener("change", displayNewCity);
